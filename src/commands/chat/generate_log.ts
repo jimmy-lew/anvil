04bc3846d086220b6aa404394a24b1df45d62742
jimmy-lew <jimmylew9779@gmail.com>
2026-01-03 19:53:27 +0800

    feat: command to generate log types

---
+++ src/commands/chat/generate_log.ts
@@ -0,0 +1,38 @@
+import type { ChatInputCommandInteraction, PermissionsString } from 'discord.js'
+
+import type { Command } from '../index'
+import { SlashCommandBuilder } from 'discord.js'
+import { logger } from '../../logger'
+import { send_message } from '../../utils'
+import { CommandDeferType } from '../index'
+
+const metadata = new SlashCommandBuilder()
+  .setName('generate_log')
+  .setDescription('Generate mock log data')
+  .setContexts(0)
+  .addStringOption(
+    opt => opt
+      .setName('option')
+      .setDescription('Option.')
+      .addChoices(
+        { name: 'info', value: 'INFO' },
+        { name: 'debug', value: 'DEBUG' },
+        { name: 'warn', value: 'WARN' },
+        { name: 'error', value: 'ERROR' },
+        { name: 'fatal', value: 'FATAL' },
+      )
+      .setRequired(true),
+  )
+  .toJSON()
+
+export default class GenerateLogCommand implements Command {
+  public defer = CommandDeferType.HIDDEN
+  public perms: PermissionsString[] = []
+  public metadata = metadata
+
+  public async execute(intr: ChatInputCommandInteraction): Promise<void> {
+    const args = intr.options.getString('option').toLowerCase()
+    logger[args](`This is a mock ${args.toUpperCase()} call`)
+    await send_message(intr, `This is a mock ${args.toUpperCase()} call`)
+  }
+}
