ad42b6f9436da39743cf10f4ec18da8953c72c3a
jimmy-lew <jimmylew9779@gmail.com>
2026-01-10 10:42:01 +0800

    refactor: move EventHandler to index

--- src/events/event_handler.ts
+++
@@ -1,6 +0,0 @@
-import type { Events } from 'discord.js'
-
-export abstract class EventHandler {
-  event_name: Events[]
-  abstract process(event_name?: Events, ..._args: any[]): Promise<void>
-}
--- src/events/index.ts
+++ src/events/index.ts
@@ -1,3 +1,46 @@
-import { EventHandler } from './event_handler'
+import type { Events } from 'discord.js'
+import { logger } from '../logger'
 
-export { EventHandler }
+interface EventError {
+  name: string
+  message: string
+  stack?: string // TODO: Implement stack parser
+  code: number
+}
+
+export interface EventRecord {
+  time?: number
+  environment?: string
+  event_name?: string
+  error?: EventError
+  duration_ms?: number
+  meta?: Record<string, any>
+}
+
+export abstract class EventHandler {
+  event_name: Events[]
+  abstract process(event_name: Events, event: EventRecord, ..._args: any[]): Promise<void>
+
+  async _process(event_name: Events, ...args: any[]): Promise<void> {
+    const start = Date.now()
+    const event: EventRecord = {
+      event_name: event_name.split(/(?=[A-Z])/).join(' ').toUpperCase(),
+      time: start,
+      environment: 'dev',
+    }
+    try {
+      await this.process(event_name, event, ...args)
+    }
+    catch (err) {
+      event.error = {
+        name: err.name,
+        message: err.message,
+        code: err.code,
+      }
+    }
+    finally {
+      event.duration_ms = Date.now() - start
+      logger.trace(event)
+    }
+  }
+}
