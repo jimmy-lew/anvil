955cf3296ff50b4fcabeda438d62ae7e288cc299
jimmy-lew <jimmylew9779@gmail.com>
2026-01-10 10:42:30 +0800

    feat: move welcome message to after system welcome message has been sent

--- src/events/message_create_handler.ts
+++ src/events/message_create_handler.ts
@@ -1,19 +1,40 @@
 import type { Message } from 'discord.js'
-import { Events } from 'discord.js'
+import type { EventRecord } from './index'
+import { Events, MessageType } from 'discord.js'
 
-import { logger } from '../logger/index.js'
-import { parse_message_object } from '../utils/message_utils.js'
+import { logger } from '../logger'
+import { can_send, parse_message_object, send_message } from '../utils'
 import { EventHandler } from './index.js'
+
+async function handleUserJoin(msg: Message) {
+  const member = msg.member
+
+  if (!can_send(msg.channel)) {
+    logger.error('Bot does not have permission to send messages in system channel')
+    return
+  }
+
+  await send_message(
+    msg.channel,
+    `Welcome to the Smithy ${member.user}!\n\nTo get started, I'd recommend going to <#1442865424994930738> to find a whole bunch of great tips to get started.\n\nFeel free to post your work for critique, ask questions on modeling, etc in <#1442713854604869672>. We're in <#703082230486138923> which serves as our general chat!\n\nYou may also see a couple of us in the VC, streaming our modeling. Feel free to join us!`,
+    1000,
+  )
+}
 
 export default class MessageHandler extends EventHandler {
   event_name = [Events.MessageCreate, Events.MessageDelete]
 
-  public async process(event: Events, msg: Message): Promise<void> {
-    // const msg = raw.partial ? await raw.fetch() : raw
+  public async process(event: Events, event_record: EventRecord, msg: Message): Promise<void> {
     const is_self = msg.author && msg.author.id === msg.client.user?.id
-    if (msg.system || is_self)
+    if (is_self)
+      return
+    if (msg.type === MessageType.UserJoin) {
+      await handleUserJoin(msg)
+      return
+    }
+    if (msg.system)
       return
     const parsed_msg = await parse_message_object(msg)
-    logger.trace(parsed_msg, event.split(/(?=[A-Z])/).join(' ').toUpperCase())
+    event_record.meta = parsed_msg
   }
 }
