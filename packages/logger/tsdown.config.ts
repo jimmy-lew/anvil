fef7506b10a99e5fca6cadfa241896a0c1ddbb98
jimmy <jimmylew9779@gmail.com>
2026-02-03 10:48:11 +0800

    build: tighter bundle for logger

--- packages/logger/tsdown.config.ts
+++ packages/logger/tsdown.config.ts
@@ -3,15 +3,44 @@ import { defineConfig } from 'tsdown'
 export default defineConfig(
   [
     {
-      entry: {
-        index: './src/index.ts',
-        worker: './src/worker.ts',
-      },
+      entry: './src/index.ts',
+      format: 'esm',
+      platform: 'node',
+
+      noExternal: [/.*/],
+
       outDir: 'dist',
+      clean: true,
+      minify: false,
+      logLevel: 'error',
+      shims: true,
+      exports: true,
+      sourcemap: true,
+      dts: true,
+
+      outputOptions: {
+        inlineDynamicImports: true,
+      },
+    },
+    {
+      entry: './src/worker.ts',
+      format: 'esm',
+      platform: 'node',
+
+      noExternal: [/.*/],
+
+      outDir: 'dist',
+      clean: true,
       minify: true,
       logLevel: 'error',
+      shims: true,
       exports: true,
+      sourcemap: true,
       dts: true,
+
+      outputOptions: {
+        inlineDynamicImports: true,
+      },
     },
   ],
 )
