0af0c85d80fde5d0025cd661de9b3cbb0ed51a09
jimmy <jimmylew9779@gmail.com>
2026-02-05 09:02:05 +0800

    build: remove docus extension and complete migration to internal

--- apps/docs/nuxt.config.ts
+++ apps/docs/nuxt.config.ts
@@ -1,12 +1,31 @@
+import { createResolver, extendViteConfig, useNuxt } from '@nuxt/kit'
+
+const { resolve } = createResolver(import.meta.url)
+
 export default defineNuxtConfig({
   modules: [
+    resolve('./modules/config'),
+    resolve('./modules/routing'),
+    resolve('./modules/css'),
+    '@nuxt/content',
     '@nuxtjs/i18n',
     '@nuxt/ui',
     '@nuxt/fonts',
+    '@nuxtjs/robots',
+    'nuxt-og-image',
     '@vueuse/nuxt',
     'motion-v/nuxt',
+    () => {
+      // Update @nuxt/content optimizeDeps options
+      extendViteConfig((config) => {
+        config.optimizeDeps ||= {}
+        config.optimizeDeps.include ||= []
+        config.optimizeDeps.include.push('@nuxt/content > slugify')
+        config.optimizeDeps.include = config.optimizeDeps.include
+          .map(id => id.replace(/^@nuxt\/content > /, 'docus > @nuxt/content > '))
+      })
+    },
   ],
-  extends: ['docus'],
   css: ['~/assets/css/main.css'],
   i18n: {
     defaultLocale: 'en',
@@ -17,5 +36,20 @@ export default defineNuxtConfig({
       code: 'fr',
       name: 'Français',
     }],
+    strategy: 'prefix',
+  },
+  hooks: {
+    'nitro:config': function (config) {
+      const nuxt = useNuxt()
+      // Defensive assignment - Ensures the appropriate properties are set
+      config.prerender = config.prerender || {}
+      config.prerender.routes = config.prerender.routes || []
+
+      const i18nOptions = nuxt.options.i18n
+
+      const localeRouteMapper = (locale: string | { code: string }) => typeof locale === 'string' ? `/${locale}` : `/${locale.code}`
+      config.prerender.routes.push(...(i18nOptions.locales?.map(localeRouteMapper) || []))
+      config.prerender.routes.push('/sitemap.xml')
+    },
   },
 })
