7616e0489d84f64f2371701bcc9077b9551d8c1a
jimmy-lew <jimmylew9779@gmail.com>
2026-02-07 17:14:43 +0800

    feat: docs aside components

---
+++ apps/docs/app/components/docs/DocsAsideHeader.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const { localePath } = useInternalI18n()
+</script>
+
+<template>
+  <header class="flex items-center justify-between min-h-(--ui-header-height) border-b border-default pl-7 pr-4">
+    <div class="flex items-center gap-2">
+      <AppHeaderLogo class="h-5 w-auto shrink-0" />
+      <div class="border-muted h-6 border-solid border-s" />
+      <ULink :to="localePath('/')" class="text-highlighted font-medium hover:text-muted">
+        Docs
+      </ULink>
+    </div>
+    <UContentSearchButton variant="subtle" size="sm" />
+  </header>
+</template>
