c4455793ec2e708d4e8b1678a13ec39ac0760303
jimmy-lew <jimmylew9779@gmail.com>
2026-02-07 10:20:08 +0800

    feat: update prose link style

--- apps/docs/app/app.config.ts
+++ apps/docs/app/app.config.ts
@@ -1,8 +1,17 @@
 export default defineAppConfig({
   ui: {
     colors: {
-      primary: 'cyan',
+      primary: 'red',
       neutral: 'zinc',
+    },
+    prose: {
+      a: {
+        base: [
+          'text-white font-medium decoration-[#62666d] hover:decoration-white underline underline-offset-[clamp(2px,.225em,6px)] decoration-[clamp(1px,.1em,3px)]',
+          'transition-colors',
+          'border-none',
+        ],
+      },
     },
   },
   seo: {
