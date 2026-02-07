ccfb7cf3114b6c02e77af498617f9c92698b7abf
jimmy-lew <jimmylew9779@gmail.com>
2026-02-07 17:14:55 +0800

    feat: docs header components

---
+++ apps/docs/app/components/docs/DocsBreadcrumb.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+function partToLabelItem(part: string) {
+  return { label: part.split('-').join(' ') }
+}
+
+const route = useRoute()
+const items = computed(() => route.path.split('/').slice(2).map(partToLabelItem))
+</script>
+
+<template>
+  <UBreadcrumb
+    :items="items"
+    :ui="{
+      linkLabel: 'first-letter:uppercase',
+    }"
+  >
+    <template #separator>
+      <span class="mx-2 text-muted">/</span>
+    </template>
+  </UBreadcrumb>
+</template>
