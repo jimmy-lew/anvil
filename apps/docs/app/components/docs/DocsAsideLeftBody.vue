ec519beaf96d0fba3b112e3b5087ee4fbc13e633
jimmy-lew <jimmylew9779@gmail.com>
2026-02-07 10:20:38 +0800

    feat: flatten contentnavigation style and implement custom icon and animation

--- apps/docs/app/components/docs/DocsAsideLeftBody.vue
+++ apps/docs/app/components/docs/DocsAsideLeftBody.vue
@@ -7,6 +7,13 @@ const navigation = inject<Ref<ContentNavigationItem[]>>('navigation')
 <template>
   <UContentNavigation
     highlight
+    trailing-icon="lucide-chevron-right"
+    :default-open="false"
     :navigation="navigation"
+    :ui="{
+      listWithChildren: 'ms-0 border-none',
+      item: 'ps-0 ms-0',
+      linkTrailingIcon: 'group-data-[state=open]:rotate-90',
+    }"
   />
 </template>
